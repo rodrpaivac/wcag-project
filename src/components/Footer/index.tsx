467c34530eafafaf73ce33517a96f92d0f25540b
Rodrigo Paiva <rodrigo.paiva@gerenciarsc.com.br>
2022-01-08 20:43:07 -0300

    footer simplified

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,46 +1,19 @@
 import React from "react";
 
-import { 
-  FooterDiv,
-  Container, 
-  Button,
-  Div,
-  DivButton,
-  Icons,
-  FacebookIcon,
-  InstagramIcon,
-  TwitterIcon,
-  GithubIcon,
-} from "./styles";
+import { FooterDiv, Container, Div } from "./styles";
 
 const Footer: React.FC = () => {
   return (
     <Container>
       <FooterDiv>
-        <Div className="logo">
-          <h1>CompanyLogo</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, esse eos quas porro provident nostrum unde rem in ratione dolore commodi id incidunt cupiditate ipsum?</p>
-        </Div>
         <Div>
-          <Icons>
-            <FacebookIcon className="icon" />
-            <InstagramIcon className="icon" />
-            <TwitterIcon className="icon" />
-            <GithubIcon className="icon" />
-          </Icons>
-        </Div>
-        <Div>
-          <h2>Contato</h2>
+          <h4>Contato</h4>
           <p>example@gmail.com</p>
           <p>(38) 98824-2257</p>
         </Div>
-        <DivButton className="signIn">
-          <Button solid>Entrar</Button>
-          <Button>Registrar-se</Button>
-        </DivButton>
       </FooterDiv>
     </Container>
-  )
+  );
 };
 
 export default Footer;
